4dc7dc8e45f8cb1dba811704af33f18a7794ba95
Andrew LC <smartandrew2003@gmail.com>
2023-04-10 20:49:37 +0530

    Logo

--- src/renderer/state/global.ts
+++ src/renderer/state/global.ts
@@ -1,8 +1,17 @@
-import { atom, selector } from "recoil";
+import { atom } from "recoil";
+
+
+const msg = `
+## Welcome To MarkOvEditor
+---
+[](../../../public/logo.jpg)
+- This is a simple Markdown and Emacs Org mode editor and viewer capable of editing and operating file commands.
+- By default it uses markdown renderer when the application is without a saved file.
+`
 
 const textEditorState = atom({
   key: 'textEditorState',
-  default: ''
+  default: msg
 })
 
 const fileExtensionState = atom({
